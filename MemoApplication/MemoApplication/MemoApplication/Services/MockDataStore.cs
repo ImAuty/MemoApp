032cfaa290c625b4348eda779b7c0813f2e80595
ImAuty <DeveloperAuty@gmail.com>
2020-06-14 02:04:00 +0900

    コメントを追加しました

--- MemoApplication/MemoApplication/MemoApplication/Services/MockDataStore.cs
+++ MemoApplication/MemoApplication/MemoApplication/Services/MockDataStore.cs
@@ -6,23 +6,36 @@ using MemoApplication.Models;
 
 namespace MemoApplication.Services
 {
+    // 表示アイテム管理クラス
     public class MockDataStore : IDataStore<Item>
     {
+        /// <summary>
+        /// 表示アイテムリスト
+        /// </summary>
         readonly List<Item> items;
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         public MockDataStore()
         {
+            // TODO: 保存したデータの取得・設定
             items = new List<Item>()
             {
-                new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
             };
         }
 
+        /// <summary>
+        /// 表示アイテムを追加する
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>True:成功、False:失敗</returns>
         public async Task<bool> AddItemAsync(Item item)
         {
             items.Add(item);
@@ -30,6 +43,11 @@ namespace MemoApplication.Services
             return await Task.FromResult(true);
         }
 
+        /// <summary>
+        /// 表示アイテムを更新する
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>True:成功、False:失敗</returns>
         public async Task<bool> UpdateItemAsync(Item item)
         {
             var oldItem = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
@@ -39,6 +57,11 @@ namespace MemoApplication.Services
             return await Task.FromResult(true);
         }
 
+        /// <summary>
+        /// 表示アイテムを削除する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<bool> DeleteItemAsync(string id)
         {
             var oldItem = items.Where((Item arg) => arg.Id == id).FirstOrDefault();
@@ -47,11 +70,21 @@ namespace MemoApplication.Services
             return await Task.FromResult(true);
         }
 
+        /// <summary>
+        /// 表示アイテムを取得する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>表示アイテム（存在しない場合はnullを返す）</returns>
         public async Task<Item> GetItemAsync(string id)
         {
             return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
         }
 
+        /// <summary>
+        /// 表示アイテムを取得する
+        /// </summary>
+        /// <param name="forceRefresh"></param>
+        /// <returns>表示アイテムリスト</returns>
         public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
         {
             return await Task.FromResult(items);
